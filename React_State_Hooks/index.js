119551a1819ca9febf08747129947db0631a02fd
Maxrio <lasse@rosendahl-energietechnik.de>
2022-03-09 20:49:17 +0100

    shifted code to React repository

--- React_State_Hooks/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./Container/AppFunction";
-// import App from "./Container/AppFunction";
-
-ReactDOM.render(
-  <App />,
-  document.getElementById("app")
-);
